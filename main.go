99733862749fadad2100129d02ad0abf9e73b6bc
hanif <hanif@appscode.com>
2017-11-02 18:58:08 +0600

    init

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"go/build"
 	"strings"
 	"github.com/sdboyer/gps/pkgtree"
+	"github.com/gps/gps"
 )
 
 func main()  {
@@ -17,9 +18,32 @@ func main()  {
 	importroot := filepath.ToSlash(strings.TrimPrefix(root, srcprefix))
 	fmt.Println("Get the root:-", root)
 	fmt.Println("Get the importroot:-", importroot)
+
+	params := gps.SolveParameters{
+		RootDir: root,
+		Trace: true,
+		TraceLogger: log.New(os.Stdout, "", 0),
+		ProjectAnalyzer: NaiveAnalyzer{},
+	}
 	pkgList, err := pkgtree.ListPackages(root, importroot)
 	if err != nil {
 		log.Fatalln("Error getting pkglist.....", err)
 	}
+	params.RootPackageTree = pkgList
 	log.Println("Hello packageList", pkgList)
+}
+
+type NaiveAnalyzer struct{}
+// DeriveManifestAndLock is called when the solver needs manifest/lock data
+// for a particular dependency project (identified by the gps.ProjectRoot
+// parameter) at a particular version. That version will be checked out in a
+// directory rooted at path.
+func (a NaiveAnalyzer) DeriveManifestAndLock(path string, n gps.ProjectRoot) (gps.Manifest, gps.Lock, error) {
+	return nil, nil, nil
+}
+
+// Reports the name and version of the analyzer. This is used internally as part
+// of gps' hashing memoization scheme.
+func (a NaiveAnalyzer) Info() (name string, version int) {
+	return "example-analyzer", 1
 }
