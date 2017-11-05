47112ce5ab64bc82689985815e2b70a0fb9d5ec2
hanif <hanif@thetigerworks.com>
2017-11-05 23:02:16 +0600

    trying manifest

--- main.go
+++ main.go
@@ -1,39 +1,81 @@
+// +build ignore
+
 package main
 
 import (
+	"fmt"
+	"go/build"
+	"io/ioutil"
 	"log"
 	"os"
-	"fmt"
 	"path/filepath"
-	"go/build"
 	"strings"
+
+	"github.com/sdboyer/gps"
 	"github.com/sdboyer/gps/pkgtree"
-	"github.com/gps/gps"
+
 )
 
-func main()  {
-	log.Println("Starting demo dep....")
+// This is probably the simplest possible implementation of gps. It does the
+// substantive work that `go get` does, except:
+//  1. It drops the resulting tree into vendor instead of GOPATH
+//  2. It prefers semver tags (if available) over branches
+//  3. It removes any vendor directories nested within dependencies
+//
+//  This will compile and work...and then blow away any vendor directory present
+//  in the cwd. Be careful!
+func main() {
+	// Assume the current directory is correctly placed on a GOPATH, and that it's the
+	// root of the project.
 	root, _ := os.Getwd()
 	srcprefix := filepath.Join(build.Default.GOPATH, "src") + string(filepath.Separator)
 	importroot := filepath.ToSlash(strings.TrimPrefix(root, srcprefix))
-	fmt.Println("Get the root:-", root)
-	fmt.Println("Get the importroot:-", importroot)
-
+	/* m := gps.SimpleManifest{
+		Deps: []gps.ProjectConstraint{
+			{
+				Ident: gps.ProjectIdentifier{
+					ProjectRoot: gps.ProjectRoot("github.com/a8uhnf/go_stack"),
+				},
+				Constraint: gps.NewBranch("master"),
+			},
+		},
+	} */
+	// params.Manifest = m
+	// Set up params, including tracing
 	params := gps.SolveParameters{
-		RootDir: root,
-		Trace: true,
-		TraceLogger: log.New(os.Stdout, "", 0),
+		RootDir:         root,
+		Trace:           false,
+		TraceLogger:     log.New(os.Stdout, "", 0),
 		ProjectAnalyzer: NaiveAnalyzer{},
+		Manifest:        ManifestYaml{},
+		RootPackageTree: pkgtree.PackageTree{
+			ImportRoot: importroot,
+		},
 	}
-	pkgList, err := pkgtree.ListPackages(root, importroot)
-	if err != nil {
-		log.Fatalln("Error getting pkglist.....", err)
+	// Perform static analysis on the current project to find all of its imports.
+	 // params.RootPackageTree, _ = pkgtree.ListPackages(root, importroot)
+
+	// Set up a SourceManager. This manages interaction with sources (repositories).
+	tempdir, _ := ioutil.TempDir("", "gps-repocache")
+	sourcemgr, _ := gps.NewSourceManager(filepath.Join(tempdir))
+	defer sourcemgr.Release()
+
+	// Prep and run the solver
+	fmt.Println("Got it never")
+	solver, err := gps.Prepare(params, sourcemgr)
+	fmt.Println("Got it", err)
+	solution, err := solver.Solve()
+	fmt.Println("Hello Error", err)
+	if err == nil {
+		// If no failure, blow away the vendor dir and write a new one out,
+		// stripping nested vendor directories as we go.
+		os.RemoveAll(filepath.Join(root, "vendor"))
+		gps.WriteDepTree(filepath.Join(root, "vendor"), solution, sourcemgr, true)
 	}
-	params.RootPackageTree = pkgList
-	log.Println("Hello packageList", pkgList)
 }
 
 type NaiveAnalyzer struct{}
+
 // DeriveManifestAndLock is called when the solver needs manifest/lock data
 // for a particular dependency project (identified by the gps.ProjectRoot
 // parameter) at a particular version. That version will be checked out in a
@@ -46,4 +88,36 @@ func (a NaiveAnalyzer) DeriveManifestAndLock(path string, n gps.ProjectRoot) (gp
 // of gps' hashing memoization scheme.
 func (a NaiveAnalyzer) Info() (name string, version int) {
 	return "example-analyzer", 1
+}
+
+type ManifestYaml struct{}
+
+func (a ManifestYaml) IgnoredPackages() map[string]bool {
+	return nil
+}
+
+func (a ManifestYaml) RequiredPackages() map[string]bool {
+	return nil
+}
+
+func (a ManifestYaml) Overrides() gps.ProjectConstraints {
+	return gps.ProjectConstraints{
+		"Hello": gps.ProjectProperties{
+			Source:     "https://github.com/a8uhnf/go_stack",
+			Constraint: gps.NewBranch("master"),
+		},
+	}
+}
+
+func (a ManifestYaml) DependencyConstraints() gps.ProjectConstraints {
+	return gps.ProjectConstraints{
+		"Hello": gps.ProjectProperties{
+			Source:     "https://github.com/a8uhnf/go_stack",
+			Constraint: gps.NewBranch("master"),
+		},
+	}
+}
+
+func (a ManifestYaml) TestDependencyConstraints() gps.ProjectConstraints {
+	return nil
 }
