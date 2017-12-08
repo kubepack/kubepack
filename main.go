cccbf5dcd97a42d77ab7cbac516c8f8bfd0a7995
hanif <hanif@appscode.com>
2017-12-08 10:27:19 +0600

    Fix root main.go file

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// +build ignore
-
 package main
 
 import (
@@ -11,9 +9,14 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/sdboyer/gps"
-	"github.com/sdboyer/gps/pkgtree"
+	"context"
+	"time"
+
 	"github.com/ghodss/yaml"
+	"github.com/golang/dep/gps"
+	"github.com/golang/dep/gps/pkgtree"
+	// "github.com/packsh/demo-dep/dep/copy"
+	typ "github.com/kubepack/pack/type"
 )
 
 // This is probably the simplest possible implementation of gps. It does the
@@ -24,27 +27,14 @@ import (
 //
 //  This will compile and work...and then blow away any vendor directory present
 //  in the cwd. Be careful!
-type ManifestDef struct {
-	Package string `json:"package"`
-	Owners  []struct {
-		Name  string `json:"name"`
-		Email string `json:"email"`
-	} `json:"owners"`
-	Dependencies []struct {
-		Package string `json:"package"`
-		Version string `json:"version,omitempty"`
-		Repo    string `json:"repo,omitempty"`
-		Branch  string `json:"branch,omitempty"`
-	} `json:"dependencies"`
-}
 
 func main() {
 	// Assume the current directory is correctly placed on a GOPATH, and that it's the
 	// root of the project.
 	root, _ := os.Getwd()
-	man := root + "/manifest.yaml"
+	man := filepath.Join(root, "manifest.yaml")
 	byt, err := ioutil.ReadFile(man)
-	manStruc := ManifestDef{}
+	manStruc := typ.ManifestDefinition{}
 	err = yaml.Unmarshal(byt, &manStruc)
 	if err != nil {
 		log.Fatalln("Error Occuered-----", err)
@@ -59,70 +49,172 @@ func main() {
 	srcprefix := filepath.Join(build.Default.GOPATH, "src") + string(filepath.Separator)
 	importroot := filepath.ToSlash(strings.TrimPrefix(root, srcprefix))
 
-	params := gps.SolveParameters{
-		RootDir:         root,
-		Trace:           false,
-		TraceLogger:     log.New(os.Stdout, "", 0),
-		ProjectAnalyzer: NaiveAnalyzer{},
-		Manifest:        ManifestYaml{},
-		RootPackageTree: pkgtree.PackageTree{
-			ImportRoot: importroot,
-			Packages: map[string]pkgtree.PackageOrErr{
-				"github.com/sdboyer/gps": pkgtree.PackageOrErr{
-					P: pkgtree.Package{
-						// Name:       "github.com/a8uhnf/go_stack",
-						// ImportPath: "github.com/packsh/demo-dep",
-						Imports: imports,
-					},
-				},
+	manifestYaml := ManifestYaml{}
+	manifestYaml.root = root
+	pkgTree := map[string]pkgtree.PackageOrErr{
+		"github.com/sdboyer/gps": {
+			P: pkgtree.Package{
+				// Name:       "github.com/a8uhnf/go_stack",
+				// ImportPath: "github.com/packsh/demo-dep",
+				Imports: imports,
 			},
 		},
 	}
-
+	params := gps.SolveParameters{
+		RootDir:         root,
+		TraceLogger:     log.New(os.Stdout, "", 0),
+		ProjectAnalyzer: NaiveAnalyzer{},
+		Manifest:        manifestYaml,
+		RootPackageTree: pkgtree.PackageTree{
+			ImportRoot: importroot,
+			Packages:   pkgTree,
+		},
+	}
 	// Set up a SourceManager. This manages interaction with sources (repositories).
 	tempdir, _ := ioutil.TempDir("", "gps-repocache")
-	sourcemgr, _ := gps.NewSourceManager(filepath.Join(tempdir))
+	srcManagerConfig := gps.SourceManagerConfig{
+		Cachedir:       filepath.Join(tempdir),
+		Logger:         log.New(os.Stdout, "", 0),
+		DisableLocking: true,
+	}
+	log.Println("hello tempdir", tempdir)
+	sourcemgr, _ := gps.NewSourceManager(srcManagerConfig)
 	defer sourcemgr.Release()
 
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Minute)
+	defer cancel()
 	// Prep and run the solver
 	solver, err := gps.Prepare(params, sourcemgr)
-	solution, err := solver.Solve()
-	fmt.Println("Hello Error", err)
+	if err != nil {
+		fmt.Println("Prepare error occurred..", err)
+		return
+	}
+	solution, err := solver.Solve(ctx)
+	if err != nil {
+		fmt.Println("Solve error occurred..", err)
+		return
+	}
 	if err == nil {
 		// If no failure, blow away the vendor dir and write a new one out,
 		// stripping nested vendor directories as we go.
-		os.RemoveAll(filepath.Join(root, "vendor"))
-		gps.WriteDepTree(filepath.Join(root, "vendor"), solution, sourcemgr, true)
+		os.RemoveAll(filepath.Join(root, "_vendor"))
+		gps.WriteDepTree(filepath.Join(root, "_vendor"), solution, sourcemgr, true, log.New(os.Stdout, "Hello:-----", 4))
 	}
 }
 
-type NaiveAnalyzer struct{}
+func DepRun() {
+	// Assume the current directory is correctly placed on a GOPATH, and that it's the
+	// root of the project.
+	root, _ := os.Getwd()
+	man := filepath.Join(root, "manifest.yaml")
+	byt, err := ioutil.ReadFile(man)
+	manStruc := typ.ManifestDefinition{}
+	err = yaml.Unmarshal(byt, &manStruc)
+	if err != nil {
+		log.Fatalln("Error Occuered-----", err)
+	}
+
+	imports := make([]string, len(manStruc.Dependencies))
+
+	for key, value := range manStruc.Dependencies {
+		imports[key] = value.Package
+	}
+
+	srcprefix := filepath.Join(build.Default.GOPATH, "src") + string(filepath.Separator)
+	importroot := filepath.ToSlash(strings.TrimPrefix(root, srcprefix))
+
+	manifestYaml := ManifestYaml{}
+	manifestYaml.root = root
+	pkgTree := map[string]pkgtree.PackageOrErr{
+		"github.com/sdboyer/gps": {
+			P: pkgtree.Package{
+				// Name:       "github.com/a8uhnf/go_stack",
+				// ImportPath: "github.com/packsh/demo-dep",
+				Imports: imports,
+			},
+		},
+	}
+	params := gps.SolveParameters{
+		RootDir:         root,
+		TraceLogger:     log.New(os.Stdout, "", 0),
+		ProjectAnalyzer: NaiveAnalyzer{},
+		Manifest:        manifestYaml,
+		RootPackageTree: pkgtree.PackageTree{
+			ImportRoot: importroot,
+			Packages:   pkgTree,
+		},
+	}
+	// Set up a SourceManager. This manages interaction with sources (repositories).
+	tempdir, _ := ioutil.TempDir("", "gps-repocache")
+	srcManagerConfig := gps.SourceManagerConfig{
+		Cachedir:       filepath.Join(tempdir),
+		Logger:         log.New(os.Stdout, "", 0),
+		DisableLocking: true,
+	}
+	log.Println("hello tempdir", tempdir)
+	sourcemgr, _ := gps.NewSourceManager(srcManagerConfig)
+	defer sourcemgr.Release()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Minute)
+	defer cancel()
+	// Prep and run the solver
+	solver, err := gps.Prepare(params, sourcemgr)
+	if err != nil {
+		fmt.Println("Prepare error occurred..", err)
+		return
+	}
+	solution, err := solver.Solve(ctx)
+	if err != nil {
+		fmt.Println("Solve error occurred..", err)
+		return
+	}
+	if err == nil {
+		// If no failure, blow away the vendor dir and write a new one out,
+		// stripping nested vendor directories as we go.
+		os.RemoveAll(filepath.Join(root, "_vendor"))
+		gps.WriteDepTree(filepath.Join(root, "_vendor"), solution, sourcemgr, true, log.New(os.Stdout, "Hello:-----", 4))
+	}
+}
+
+type NaiveAnalyzer struct {
+	// lookForManifest(root string) (gps.)
+}
 
 // DeriveManifestAndLock is called when the solver needs manifest/lock data
 // for a particular dependency project (identified by the gps.ProjectRoot
 // parameter) at a particular version. That version will be checked out in a
 // directory rooted at path.
 func (a NaiveAnalyzer) DeriveManifestAndLock(path string, n gps.ProjectRoot) (gps.Manifest, gps.Lock, error) {
-	fmt.Println("Hello path", path)
-	fmt.Println("Hello n", n)
-
+	// man := filepath.Join(filepath.Join(path, "manifest.yaml"))
+	// return nil, nil, nil
 	// this check should be unnecessary, but keeping it for now as a canary
 	if _, err := os.Lstat(path); err != nil {
 		return nil, nil, fmt.Errorf("No directory exists at %s; cannot produce ProjectInfo", path)
 	}
 
-	return nil, nil, nil
+	m, l, err := a.lookForManifest(path)
+	if err == nil {
+		// TODO verify project name is same as what SourceManager passed in?
+		return m, l, nil
+	} else {
+		return nil, nil, err
+	}
 }
 
 // Reports the name and version of the analyzer. This is used internally as part
 // of gps' hashing memoization scheme.
-func (a NaiveAnalyzer) Info() (name string, version int) {
-	return "example-analyzer", 1
+func (a NaiveAnalyzer) Info() gps.ProjectAnalyzerInfo {
+	return gps.ProjectAnalyzerInfo{
+		Name:    "example-analyzer",
+		Version: 1,
+	}
 }
 
-type ManifestYaml struct{}
+type ManifestYaml struct {
+	root string
+}
 
-func (a ManifestYaml) IgnoredPackages() map[string]bool {
+func (a ManifestYaml) IgnoredPackages() *pkgtree.IgnoredRuleset {
 	return nil
 }
 
@@ -131,28 +223,125 @@ func (a ManifestYaml) RequiredPackages() map[string]bool {
 }
 
 func (a ManifestYaml) Overrides() gps.ProjectConstraints {
-	// return nil
+	fmt.Println("Hello world!!!!--------", a.root)
+	ovrr := gps.ProjectConstraints{}
+
+	mpath := filepath.Join(a.root, typ.ManifestFile)
+	byt, err := ioutil.ReadFile(mpath)
+	manStruc := typ.ManifestDefinition{}
+	err = yaml.Unmarshal(byt, &manStruc)
+	if err != nil {
+		log.Fatalln("Error Occuered-----", err)
+	}
+
+	for _, value := range manStruc.Dependencies {
+		properties := gps.ProjectProperties{}
+		if value.Repo != "" {
+			properties.Source = value.Repo
+		} else {
+			properties.Source = value.Package
+		}
+		if value.Branch != "" {
+			properties.Constraint = gps.NewBranch(value.Branch)
+		} else if value.Version != "" {
+			properties.Constraint = gps.Revision(value.Version)
+		}
+		ovrr[gps.ProjectRoot(value.Package)] = properties
+	}
+	return ovrr
 	return gps.ProjectConstraints{
-		"github.com/Masterminds/semver": gps.ProjectProperties{
-			Source:     "github.com/Masterminds/semver",
-			Constraint: gps.NewBranch("2.x"),
+		gps.ProjectRoot("github.com/a8uhnf/test-yml"): gps.ProjectProperties{
+			Source:     "github.com/a8uhnf/test-yml",
+			Constraint: gps.NewVersion("0.0.1"),
 		},
 	}
+	return nil
 }
 
 func (a ManifestYaml) DependencyConstraints() gps.ProjectConstraints {
-	return gps.ProjectConstraints{
-		"github.com/appscode/go": gps.ProjectProperties{
-			Source:     "github.com/appscode/go",
-			Constraint: gps.NewBranch("master"),
-		},
-		"github.com/Masterminds/semver": gps.ProjectProperties{
-			Source:     "github.com/Masterminds/semver",
-			Constraint: gps.NewBranch("2.x"),
-		},
+	projectConstraints := make(gps.ProjectConstraints)
+
+	man := filepath.Join(a.root, typ.ManifestFile)
+	byt, err := ioutil.ReadFile(man)
+	manStruc := typ.ManifestDefinition{}
+	err = yaml.Unmarshal(byt, &manStruc)
+	if err != nil {
+		log.Fatalln("Error Occuered-----", err)
 	}
+
+	for _, value := range manStruc.Dependencies {
+		properties := gps.ProjectProperties{}
+		if value.Repo != "" {
+			properties.Source = value.Repo
+		} else {
+			properties.Source = value.Package
+		}
+		if value.Branch != "" {
+			properties.Constraint = gps.NewBranch(value.Branch)
+		} else if value.Version != "" {
+			properties.Constraint = gps.Revision(value.Version)
+		}
+		projectConstraints[gps.ProjectRoot(value.Package)] = properties
+	}
+	return projectConstraints
 }
 
 func (a ManifestYaml) TestDependencyConstraints() gps.ProjectConstraints {
 	return nil
+}
+
+type InternalManifest struct {
+	root string
+}
+
+func (a InternalManifest) DependencyConstraints() gps.ProjectConstraints {
+	projectConstraints := make(gps.ProjectConstraints)
+
+	man := filepath.Join(a.root, typ.ManifestFile)
+	byt, err := ioutil.ReadFile(man)
+	manStruc := typ.ManifestDefinition{}
+	err = yaml.Unmarshal(byt, &manStruc)
+	if err != nil {
+		log.Fatalln("Error Occuered-----", err)
+	}
+
+	for _, value := range manStruc.Dependencies {
+		properties := gps.ProjectProperties{}
+		if value.Repo != "" {
+			properties.Source = value.Repo
+		} else {
+			properties.Source = value.Package
+		}
+		if value.Branch != "" {
+			properties.Constraint = gps.NewBranch(value.Branch)
+		} else if value.Version != "" {
+			properties.Constraint = gps.Revision(value.Version)
+		}
+		projectConstraints[gps.ProjectRoot(value.Package)] = properties
+	}
+	return projectConstraints
+}
+
+type InternalLock struct {
+	root string
+}
+
+func (a InternalLock) Projects() []gps.LockedProject {
+	return nil
+}
+
+func (a InternalLock) InputsDigest() []byte {
+	return nil
+}
+
+func (a NaiveAnalyzer) lookForManifest(root string) (gps.Manifest, gps.Lock, error) {
+	mpath := filepath.Join(root, typ.ManifestFile)
+	if _, err := os.Lstat(mpath); err != nil {
+		return nil, nil, err
+	}
+	man := &InternalManifest{}
+	man.root = root
+	lck := &InternalLock{}
+	lck.root = root
+	return man, lck, nil
 }
