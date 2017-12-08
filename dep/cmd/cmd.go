5f9ab59c2cc5f2cf360365aa74097dbe04494bf7
hanif <hanif@appscode.com>
2017-12-08 13:20:04 +0600

    Clean up

--- dep/cmd/cmd.go
+++ dep/cmd/cmd.go
@@ -5,19 +5,6 @@ import (
 )
 
 func NewDemoDepCmd() *cobra.Command {
-	/*root, err := os.Getwd()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	repo, err := vcs.NewRepo("", root)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	crnt, err := repo.Current()
-	commitInfo, err := repo.CommitInfo(string(crnt))
-	if err != nil {
-		log.Fatalln(err)
-	}*/
 	cmds := &cobra.Command{
 		Use:   "pack",
 		Short: "Cli for demo dep",
