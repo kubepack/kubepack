af410ff2e9d2be0b4dae01500b9d0c6073272d38
Tamal Saha <tamal@appscode.com>
2020-04-15 23:39:05 -0700

    Generate scripts per supported os (#90)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/install-yaml-generator/main.go
+++ cmd/install-yaml-generator/main.go
@@ -17,6 +17,7 @@ limitations under the License.
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -54,9 +55,13 @@ func main() {
 	}
 	order.UID = types.UID(uuid.New().String())
 
-	script, err := lib.GenerateYAMLScript(bs, lib.DefaultRegistry, order)
+	scripts, err := lib.GenerateYAMLScript(bs, lib.DefaultRegistry, order)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(script)
+	data, err = json.MarshalIndent(scripts, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
 }
