758392ea713da07a65593663be203b3650523e2f
Tamal Saha <tamal@appscode.com>
2020-04-27 21:15:49 -0700

    Use improved GetChangedValues
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/kubepack.dev/lib-helm/chart/lib.go
+++ vendor/kubepack.dev/lib-helm/chart/lib.go
@@ -1,77 +1,144 @@
 package chart
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
-
-	"github.com/google/go-cmp/cmp"
-	"github.com/pkg/errors"
-	"helm.sh/helm/v3/pkg/chart"
 )
 
-func GetChangedValues(original map[string]interface{}, modified map[string]interface{}) []string {
-	var cmd []string
-	getChangedValues(original, modified, "", &cmd)
-	return cmd
+func GetChangedValues(original, modified map[string]interface{}) ([]string, error) {
+	cmds, err := getChangedValues(original, modified, "", nil)
+	if err != nil {
+		return nil, err
+	}
+	sort.Strings(cmds)
+	return cmds, nil
 }
 
-func getChangedValues(original map[string]interface{}, modified map[string]interface{}, currentKey string, setCmds *[]string) {
-	for key, val := range original {
-		tempKey := ""
-		if currentKey == "" {
-			tempKey = key
+func getChangedValues(original, modified map[string]interface{}, prefix string, cmds []string) ([]string, error) {
+	for k, v := range modified {
+		curKey := ""
+		if prefix == "" {
+			curKey = escapeKey(k)
 		} else {
-			tempKey = currentKey + "." + key
+			curKey = prefix + "." + escapeKey(k)
 		}
 
-		switch val := val.(type) {
+		switch val := v.(type) {
 		case map[string]interface{}:
-			getChangedValues(val, modified[key].(map[string]interface{}), tempKey, setCmds)
-		case []interface{}:
-			if !cmp.Equal(val, modified[key]) {
-				tempCmd := tempKey + "=["
-				arrayLen := len(modified[key].([]interface{}))
-				for i, element := range modified[key].([]interface{}) {
-					tempCmd = tempCmd + fmt.Sprintf("%v", element)
-					if i != arrayLen-1 {
-						tempCmd = tempCmd + fmt.Sprintf(",")
-					}
-				}
-				tempCmd = tempCmd + "]"
-				if strings.Contains(tempCmd, "=[]") {
-					tempCmd = strings.ReplaceAll(tempCmd, "[]", "null")
-				}
-				*setCmds = append(*setCmds, tempCmd)
+			oVal, ok := original[k].(map[string]interface{})
+			if !ok {
+				oVal = map[string]interface{}{}
 			}
-		case interface{}:
-			if val != modified[key] {
-				if isZeroOrNil(modified[key]) {
-					*setCmds = append(*setCmds, fmt.Sprintf("%s=null ", tempKey))
-				} else {
-					*setCmds = append(*setCmds, fmt.Sprintf("%s=%v ", tempKey, modified[key]))
+			next, err := getChangedValues(oVal, val, curKey, cmds)
+			if err != nil {
+				return nil, err
+			}
+			cmds = append(cmds, next...)
+		case []interface{}:
+			if !reflect.DeepEqual(v, original[k]) {
+				if len(val) == 0 {
+					cmds = append(cmds, fmt.Sprintf("%s=null", curKey))
+					continue
 				}
+
+				if isSimpleArray(val) {
+					s, err := PrintArray(val)
+					if err != nil {
+						return nil, fmt.Errorf("failed to print simple array %v, reason: %v", v, err)
+					}
+					cmds = append(cmds, fmt.Sprintf("%s=%s", curKey, s))
+					continue
+				}
+
+				for i, element := range val {
+					em, ok := element.(map[string]interface{})
+					if !ok {
+						return nil, fmt.Errorf("%s[%d] element is not a map", curKey, i)
+					}
+					next, err := getChangedValues(map[string]interface{}{}, em, fmt.Sprintf("%s[%d]", curKey, i), cmds)
+					if err != nil {
+						return nil, err
+					}
+					cmds = append(cmds, next...)
+				}
+			}
+		case string:
+			if !reflect.DeepEqual(original[k], val) {
+				cmds = append(cmds, fmt.Sprintf("%s=%v", curKey, escapeValue(val)))
+			}
+		case int8, uint8, int16, uint16, int32, uint32, int64, uint64, int, uint, float32, float64, bool, json.Number:
+			if !reflect.DeepEqual(original[k], val) {
+				cmds = append(cmds, fmt.Sprintf("%s=%v", curKey, val))
+			}
+		case nil:
+			if !reflect.DeepEqual(original[k], val) {
+				cmds = append(cmds, fmt.Sprintf("%s=null", curKey))
 			}
 		default:
-			if val != modified[key] {
-				if isZeroOrNil(modified[key]) {
-					*setCmds = append(*setCmds, fmt.Sprintf("%s=null ", tempKey))
-				} else {
-					*setCmds = append(*setCmds, fmt.Sprintf("%s=%v ", tempKey, modified[key]))
-				}
-			}
+			return nil, fmt.Errorf("unknown type %v with value %v", reflect.TypeOf(v), v)
 		}
 	}
+	return cmds, nil
 }
 
-func isZeroOrNil(x interface{}) bool {
-	return x == nil || reflect.DeepEqual(x, reflect.Zero(reflect.TypeOf(x)).Interface())
-}
-
-func isChartInstallable(ch *chart.Chart) (bool, error) {
-	switch ch.Metadata.Type {
-	case "", "application":
-		return true, nil
+// kubernetes.io/role becomes "kubernetes\.io/role"
+func escapeKey(s string) string {
+	if !strings.ContainsRune(s, '.') {
+		return s
 	}
-	return false, errors.Errorf("%s charts are not installable", ch.Metadata.Type)
+	return `"` + strings.ReplaceAll(strings.ReplaceAll(s, `\`, `\\`), `.`, `\.`) + `"`
+}
+
+// "value1,value2" becomes value1\,value2
+func escapeValue(s string) string {
+	return strings.ReplaceAll(strings.ReplaceAll(s, `\`, `\\`), `,`, `\,`)
+}
+
+func isSimpleArray(a []interface{}) bool {
+	for i := range a {
+		switch a[i].(type) {
+		case string, int8, uint8, int16, uint16, int32, uint32, int64, uint64, int, uint, float32, float64, bool, nil, json.Number:
+		default:
+			return false
+		}
+	}
+	return true
+}
+
+func PrintArray(a []interface{}) (string, error) {
+	var buf bytes.Buffer
+	buf.WriteRune('{')
+	for i := range a {
+		switch v := a[i].(type) {
+		case string:
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			_, err := fmt.Fprint(&buf, escapeValue(v))
+			if err != nil {
+				return "", err
+			}
+		case int8, uint8, int16, uint16, int32, uint32, int64, uint64, int, uint, float32, float64, bool, json.Number:
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			_, err := fmt.Fprint(&buf, v)
+			if err != nil {
+				return "", err
+			}
+		case nil:
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			buf.WriteString("null")
+		default:
+			return "", fmt.Errorf("[%d] holds a complex type %v", i, reflect.TypeOf(a[i]))
+		}
+	}
+	buf.WriteRune('}')
+	return buf.String(), nil
 }
