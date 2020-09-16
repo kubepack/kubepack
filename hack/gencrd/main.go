053951bb38051ca619ffda06f1a63c270f5f978e
lgtm <1gtm@users.noreply.github.com>
2020-09-15 18:20:57 -0700

    Update Kubernetes v1.18.3 dependencies (#131)
    Signed-off-by: 1gtm <1gtm@appscode.com>
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/gencrd/main.go
+++ hack/gencrd/main.go
@@ -48,13 +48,17 @@ func generateSwaggerJson() {
 			Title:   "Kubepack",
 			Version: "v0.2.0",
 			Contact: &spec.ContactInfo{
-				Name:  "AppsCode Inc.",
-				URL:   "https://appscode.com",
-				Email: "support@appscode.com",
+				ContactInfoProps: spec.ContactInfoProps{
+					Name:  "AppsCode Inc.",
+					URL:   "https://appscode.com",
+					Email: "support@appscode.com",
+				},
 			},
 			License: &spec.License{
-				Name: "Apache 2.0",
-				URL:  "https://www.apache.org/licenses/LICENSE-2.0.html",
+				LicenseProps: spec.LicenseProps{
+					Name: "Apache 2.0",
+					URL:  "https://www.apache.org/licenses/LICENSE-2.0.html",
+				},
 			},
 		},
 		OpenAPIDefinitions: []common.GetOpenAPIDefinitions{
