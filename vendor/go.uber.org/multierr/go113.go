3d5ba20fa5c2d12dd383039f3623bc1627ceea8a
lgtm <1gtm@users.noreply.github.com>
2021-01-23 05:50:02 -0800

    Update Kubernetes v1.18.9 dependencies (#165)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/go.uber.org/multierr/go113.go
+++ vendor/go.uber.org/multierr/go113.go
@@ -18,11 +18,35 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// +build tools
+// +build go1.13
 
-package atomic
+package multierr
 
-import (
-	// Tools used during development.
-	_ "golang.org/x/lint/golint"
-)
+import "errors"
+
+// As attempts to find the first error in the error list that matches the type
+// of the value that target points to.
+//
+// This function allows errors.As to traverse the values stored on the
+// multierr error.
+func (merr *multiError) As(target interface{}) bool {
+	for _, err := range merr.Errors() {
+		if errors.As(err, target) {
+			return true
+		}
+	}
+	return false
+}
+
+// Is attempts to match the provided error against errors in the error list.
+//
+// This function allows errors.Is to traverse the values stored on the
+// multierr error.
+func (merr *multiError) Is(target error) bool {
+	for _, err := range merr.Errors() {
+		if errors.Is(err, target) {
+			return true
+		}
+	}
+	return false
+}
