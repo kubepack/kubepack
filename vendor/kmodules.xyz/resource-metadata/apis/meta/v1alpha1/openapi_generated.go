da01dc16918022747cec7e871e14560ec3da4e8f
1gtm <1gtm@appscode.com>
2025-05-03 03:05:54 +0000

    Update for release ACE@v2025.5.16
    ProductLine: ACE
    
    Release: v2025.5.16
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kmodules.xyz/resource-metadata/apis/meta/v1alpha1/openapi_generated.go
+++ vendor/kmodules.xyz/resource-metadata/apis/meta/v1alpha1/openapi_generated.go
@@ -16941,6 +16941,12 @@ func schema_kmodulesxyz_client_go_api_v1_ClusterMetadata(ref common.ReferenceCal
 							Format: "",
 						},
 					},
+					"cloudServiceAuthMode": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
 				},
 				Required: []string{"uid"},
 			},
